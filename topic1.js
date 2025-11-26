c3a6b6f2a66321140ee0b5ed5dcaf506328f0b86
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2025-11-26 11:27:04 +0200

    add: lesson1

---
+++ topic1.js
@@ -0,0 +1,163 @@
+//console.log("Hello, World!");
+/* 
+coment
+coment
+coment
+*/
+// console.log("hello 2"); // console.log message
+
+//comment line
+// console.log("hello 3");
+// console.log("hello 3");
+// console.log("hello 3");
+
+// змінні
+// let const var
+
+// let message = "hello";
+
+// message = "hi";
+
+// console.log(message);
+// console.log(message);
+
+
+// const name = "John";
+
+// name = "Doe";
+
+// console.log(name);
+
+// типи даних
+
+// String
+
+// "masfsafadgewrgewgweg353546536335235dfe"
+
+// Number, BigInt
+// 34534534n
+
+// 4575757.3434
+
+// Boolean
+
+// true false
+
+// undefined
+
+// let age;
+
+// console.log(age);
+
+// null
+
+// let age = 20;
+
+// age = null;
+// console.log(age);
+
+// Symbol
+
+// Object
+
+// let user = {
+//     name: "John",
+//     age: 30,
+//     isAdmin: true
+// }
+
+// let message = "Hello, World!";
+
+// message = 45;
+// message = true;
+
+// console.log(message);
+// console.log(message);
+// console.log(typeof(message));
+
+
+// alert("Hello, World!");
+
+// prompt("Enter your name: ");
+
+// let name = prompt("Enter your name: ");
+
+// console.log(name);
+
+// let userNameOfUser
+
+// const PI
+
+
+// console.log(10 + 10)
+// console.log(10 - 10)
+// console.log(10 * 10)
+// console.log(10 / 10)
+
+// console.log(10 % 4);
+
+// console.log(2 ** 2);
+
+// let number = 10;
+
+// number += 5; // number = number + 5
+// number -= 3; // number = number - 3
+// number *= 2; // number = number * 2
+// number /= 4; // number = number / 4
+
+// console.log("Hello" + "hello");
+
+// console.log("hello" + "5");
+// console.log("5" + 5);
+
+// console.log(true + "5");
+
+// console.log("fdfd" / 2);
+
+
+// let age = prompt("Enter your age: ");
+
+// alert("Your age is " + age);
+
+// const text = "fasfasfsafs5.3434";
+
+// // const number = Number(text);
+// // const number = parseInt(text);
+// const number = parseFloat(text);
+
+// console.log(number);
+// String(number);
+// Boolean(number);
+
+
+// console.log(11 == 10);
+// console.log(11 != 10);
+// console.log(11 > 10);
+// console.log(11 >= 10);
+// console.log(11 < 10);
+// console.log(11 <= 10);
+
+// console.log(10 == "10");
+// console.log(10 === "10");
+
+// логічні оператори
+
+// && - and
+// || - or
+// ! - not
+
+// const age = 101;
+
+// console.log(age >= 18 && age <= 30);
+// console.log(age <= 0 && age >= 100);
+
+// тернарний оператор
+
+// const age = 20
+// const age = prompt("Enter your age: ");
+
+// // (умова) ? вираз1 : вираз2
+
+// let message = (age >= 18) ? "вхід дозволено" : "вхід заборонено"
+
+// console.log(message);
