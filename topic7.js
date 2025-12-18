a898f0551fc43cbca91a0c3be1b36d68f0a34ef1
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2025-12-18 16:51:59 +0200

    add:lesson

---
+++ topic7.js
@@ -0,0 +1,24 @@
+// let str = "Hello world!";
+
+// let regExp = /world/;
+
+// console.log(/world/.test("Hello world!"));
+
+// console.log("hello world!".match(/world/));
+
+// console.log("hello world World!".match(/world/g));
+
+// console.log("hello world World!".match(/world/gi));
+
+// console.log("hello world".match(/[lwjhbki]/g));
+// console.log("Hello World".match(/[a-z]/g));
+// console.log("Hello World".match(/[A-Z]/g));
+
+// console.log("Hello1 World2".match(/[0-9a-h]/gi));
+// console.log("Hello1 World2".match(/[^0-9]/gi));
+
+// console.log("Paragraph 242424234.22242424".match(/\d+\.?\d+/));
+
+
+
+
---
+++ topic8.js
@@ -0,0 +1,24 @@
+// const sayHello = function(){
+//     console.log("Hello from topic 8!");
+// }
+
+// const sayHello = () => {
+//     console.log("Hello from topic 8!");
+// }
+
+// sayHello();
+
+// const sum = function(a, b) {
+//     return a + b;
+// }
+
+// const sum = (a, b) => a + b;
+// const sum = a => a + 1;
+
+// [1, 2, 3].map(function(num) {
+//     return num * 2; 
+// });
+// [1, 2, 3].map(num => num * 2)
+//          .filter(num => num > 2)
+//          .reduce((acc, num) => acc + num, 0);
+
